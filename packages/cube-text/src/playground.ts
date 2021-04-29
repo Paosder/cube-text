5c36cbf130336c1b329b515233b9a087705caa52
Paosder <denjaraos@gmail.com>
2021-04-29 19:58:01 +0900

    modify readme

--- packages/cube-text/src/playground.ts
+++ packages/cube-text/src/playground.ts
@@ -4,17 +4,11 @@ import {
   generateGradientColor,
   generateRandomColor,
   generateRandomPosition,
-  generateRotateCameraUp,
   generateRotateY,
   generateZoom,
   randomRotate,
 } from "./cube-text";
-import {
-  generateNoisy,
-  generateRewindToOrigin,
-  generateRotateCube,
-  generateRotateCubeTo,
-} from "./cube-text/plugins/render";
+import { generateRewindToOrigin } from "./cube-text/plugins/render";
 
 const style = document.createElement("style");
 document.body.style.margin = "0";
@@ -42,60 +36,25 @@ cubeText.register(
   "init-cube",
   generateRandomPosition({ basis: true, min: [-1000, -1000, -1000] })
 );
-// cubeText.register("init-cube", generateGradientColor());
-// cubeText.register(
-//   "render",
-//   generateRotateCubeTo({
-//     duration: 3000,
-//   })
-// );
-// cubeText.register("render", generateRotateCube(4, 3000, true, true));
+cubeText.register("init-cube", generateGradientColor());
 cubeText.register("render", generateRewindToOrigin({ duration: 5000 }));
 cubeText.register("render-camera", generateFullscreen());
-// cubeText.register(
-//   "render-camera",
-//   generateRotateY({
-//     duration: 3000,
-//   })
-// );
-// cubeText.register(
-//   "render-camera",
-//   generateRotateCameraUp({
-//     cycle: 2,
-//   })
-// );
-// cubeText.register(
-//   "render-camera",
-//   generateZoom({
-//     init: 0.1,
-//     targetRatio: 1,
-//     duration: 3000,
-//   })
-// );
-// cubeText.register(
-//   "render",
-//   generateRotateCube({ radius: 0.5, individual: false })
-// );
+cubeText.register(
+  "render-camera",
+  generateRotateY({
+    duration: 5000,
+    loop: false,
+  })
+);
+cubeText.register(
+  "render-camera",
+  generateZoom({
+    init: 0.1,
+    targetRatio: 1,
+    duration: 5000,
+  })
+);
 const drawText = "Hello CubeText!";
-cubeText.drawText(drawText, { size: 12 }, { size: 1, margin: 1 });
-
-// let i = 1;
-// const arr = "Hello CubeText!".split("");
-// const interval = () => {
-//   if (i < 48) {
-//     if (i <= arr.length) {
-//       drawText += arr[i - 1];
-//     }
-//     i += 1;
-//     cubeText.drawText(drawText, {
-//       size: 49 - i,
-//     });
-//     // cubeText.run();
-//   } else if (i === 48) {
-//     cubeText.clearText();
-//     clearInterval(t);
-//   }
-// };
-// const t = setInterval(interval, 300);
+cubeText.drawText(drawText, { size: 16 }, { size: 1, margin: 1 });
 
 cubeText.run();
